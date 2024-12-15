1ead4376162ebed03da3fc86079c8a6509481998
Aeriel Denmark <aidenmark1@icloud.com>
2024-12-15 02:16:56 -0500

    Current progress for module6 with troubleshooting

--- app_admin/src/app/services/trip-data.service.ts
+++ app_admin/src/app/services/trip-data.service.ts
@@ -7,23 +7,14 @@ import { Trip } from '../models/trip';
 @Injectable({
   providedIn: 'root',
 })
+
 export class TripDataService {
+
   constructor(private http: HttpClient) {}
-  url = 'http://localhost:3000/api/trips';
 
   getTrips(): Observable<Trip[]> {
-    return this.http.get<Trip[]>(this.url);
-  }
+    let url = 'http://localhost:3000/api/trips';
 
-  addTrip(formData: Trip): Observable<Trip> {
-    return this.http.post<Trip>(this.url, formData);
-  }
-
-  getTrip(tripCode: string): Observable<Trip[]> {
-    return this.http.get<Trip[]>(this.url + '/' + tripCode);
-  }
-
-  updateTrip(formData: Trip): Observable<Trip[]> {
-    return this.http.put<Trip[]>(this.url + '/' + formData.code, formData);
+    return this.http.get<Trip[]>(url);
   }
 }
